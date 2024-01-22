5c607028c65b17957058d0efc3e38801abdde719
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-01-23 03:57:47 +0530

    wrap class and optimize code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const SteamID = require('steamid');
 const xml2js = require('xml2js');
 const NodeCache = require("node-cache");
 
-const { getUserInfo } = require('./steamFunctions');
 const myCache = new NodeCache();
 
 const app = express();
@@ -17,6 +16,100 @@ app.use(express.urlencoded({ extended: true }));
 
 const steamApiKey = process.env.STEAM_API_KEY;
 
+class Steam{
+    constructor(input){
+        this.input = input;
+        this.steamid = null;
+        this.profileData = {};
+    }
+
+    async initSteamId()
+    {
+        try{
+
+            this.steamid = this.input.includes('steamcommunity.com') 
+            ? new SteamID(await this.fromProfileToId(this.input))
+            : new SteamID(this.input);
+
+            if(!this.steamid.isValid())
+			    throw new Error("Enter valid steamid!");
+
+            this.profileData.steamID3 = this.steamid.getSteam3RenderedID()
+            this.profileData.steamID2 = this.steamid.getSteam2RenderedID()
+            this.profileData.steamID64 = this.steamid.getSteamID64()
+            
+            return this
+        }
+        catch(err)
+        {
+            throw err;
+        }
+    }
+    
+    async fromProfileToId(link) {
+        const steamIDRegex = /steamcommunity\.com\/profiles\/([0-9]+)/;
+        const match = link.match(steamIDRegex);
+    
+        if (match) {
+            return match[1];
+        }
+    
+        const vanityRegex = /steamcommunity\.com\/id\/(.+)/;
+        const vanityMatch = link.match(vanityRegex);
+    
+        if (!vanityMatch) {
+            throw new Error('Invalid Steam profile link');
+        }
+    
+        const url = `https://steamcommunity.com/id/${vanityMatch[1]}/?xml=1`;
+        const response = await fetch(url);
+        const body = await response.text();
+    
+        try {
+            const result = await xml2js.parseStringPromise(body);
+    
+            if (result.profile && result.profile.steamID64) {
+                return result.profile.steamID64[0];
+            } else if (result.response && result.response.error) {
+                throw new Error(result.response.error[0]);
+            } else {
+                throw new Error('Error in parsing data from profile!');
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    }
+    
+
+    async getUserInfo(API_KEY) {
+
+        // if (!this.profileData.steamID64) {
+        //     throw new Error('steamID64 is missing in profileData');
+        // }
+        
+        const apiUrl = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${API_KEY}&steamids=${this.profileData.steamID64}`;
+    
+        try {
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+    
+            const userData = await response.json();
+            this.profileData.profile = userData.response.players[0];
+
+            return this
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+}
+
+
 // Main Code
 app.get("/", (req, res) => {
 	res.status(200).json({ message: "Welcome to Steam Finder Rest API", version: 1.2 });
@@ -26,39 +119,23 @@ app.get("/", (req, res) => {
 // POST endpoint for SteamID conversion
 app.post('/search', async (req, res) => {
 	const input = req.body.steamid;
-
-	if (myCache.has("data_" + input)) {
-		console.log("Cache Data Get");
+    
+    if (!input) return res.status(200).json({ message: "Steamid field is missing!" });
+	
+    if (myCache.has("data_" + input)) {
+		// console.log("Cache Data Get");
 		return res.status(200).json(myCache.get("data_" + input));
 	}
 
-	if (!input) return res.status(200).json({ message: "Steamid field is missing!" });
 
 	try {
+        const result = new Steam(input)
+        await result.initSteamId()
+        await result.getUserInfo(steamApiKey)
 
-		let steamID;
-		if (input.includes('steamcommunity.com')) {
+        response = result.profileData 
 
-			const steamID64 = await getSteamIDFromProfileLinkV2(input);
-			steamID = new SteamID(steamID64);
-		} else {
-			steamID = new SteamID(input);
-		}
-
-		if(!steamID.isValid())
-			throw new Error("Enter valid steamid!");
-
-		const details = await getUserInfo(steamID.getSteamID64(), steamApiKey);
-        // console.log(details);
-
-		const response = {
-			steamID3: steamID.getSteam3RenderedID(),
-			steamID2: steamID.getSteam2RenderedID(),
-			steamID64: steamID.getSteamID64(),
-			profile: details.response.players[0]
-		}
-
-		console.log("Cache Data Set")
+        // console.log("Cache Data Set")
 		myCache.set("data_" + input, response);
 		// console.log("Cache Keys: ", myCache.keys());
 
@@ -69,86 +146,7 @@ app.post('/search', async (req, res) => {
 	}
 });
 
-// Old Fn
-/*
-function getSteamIDFromProfileLinkV1(link) {
-    return new Promise((resolve, reject) => {
-        const parser = new xml2js.Parser();
 
-        const steamIDRegex = new RegExp(/steamcommunity\.com\/profiles\/([0-9]+)/);
-
-        const match = link.match(steamIDRegex);
-        if (match) {
-            resolve(match[1]);
-        } else {
-            const vanityRegex = new RegExp(/steamcommunity\.com\/id\/(.+)/);
-
-            const vanityMatch = link.match(vanityRegex);
-            if (vanityMatch) {
-                const url = `https://steamcommunity.com/id/${vanityMatch[1]}/?xml=1`;
-                const request = require('request');
-
-                request(url, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        parser.parseString(body, function (err, result) {
-                            if (result.profile.steamID64[0]) {
-                                // return steamid64
-                                resolve(result.profile.steamID64[0]);
-                            }
-                            else if (result.response.error) {
-                                reject(new Error(result.response.error[0]));
-                            }
-                            else {
-                                reject(new Error("Error in parsing data from profile!"));
-                            }
-                        });
-                    } else {
-                        reject(new Error(error));
-                    }
-                });
-            } else {
-                reject(new Error('Invalid Steam profile link'));
-            }
-        }
-    });
-}
-*/
-
-// New Fn
-async function getSteamIDFromProfileLinkV2(link) {
-    try {
-        const steamIDRegex = /steamcommunity\.com\/profiles\/([0-9]+)/;
-        const match = link.match(steamIDRegex);
-
-        if (match) {
-            return match[1];
-        }
-
-        const vanityRegex = /steamcommunity\.com\/id\/(.+)/;
-        const vanityMatch = link.match(vanityRegex);
-
-        if (vanityMatch) {
-            const url = `https://steamcommunity.com/id/${vanityMatch[1]}/?xml=1`;
-            const response = await fetch(url);
-            const body = await response.text();
-
-            const result = await xml2js.parseStringPromise(body);
-
-            if (result.profile && result.profile.steamID64) {
-                return result.profile.steamID64[0];
-            } else if (result.response && result.response.error) {
-                throw new Error(result.response.error[0]);
-            } else {
-                throw new Error("Error in parsing data from profile!");
-            }
-        } else {
-            throw new Error('Invalid Steam profile link');
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
-        throw error;
-    }
-}
 
 
 // Start the server
